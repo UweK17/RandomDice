364b8e90a6a9e9f05fa85c3d098c8caa5aa1e67b
Uwe Kremer <uwekremer1971@gmx.de>
2022-06-25 14:18:48 +0200

    lyout

--- wurf.js
+++ wurf.js
@@ -3,13 +3,13 @@ var player1 = "Player 1";
 var player2 = "Player 2";
 
 // Function to change the player name
-function editNames() {
-    player1 = prompt("Change Player1 name");
-    player2 = prompt("Change player2 name");
+// function editNames() {
+//     player1 = prompt("Change Player1 name");
+//     player2 = prompt("Change player2 name");
 
-    document.querySelector("p.Player1").innerHTML = player1;
-    document.querySelector("p.Player2").innerHTML = player2;
-}
+//     document.querySelector("p.Player1").innerHTML = player1;
+//     document.querySelector("p.Player2").innerHTML = player2;
+// }
 
 // Function to roll the dice
 function rollTheDice() {
